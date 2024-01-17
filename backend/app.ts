561a5a1315414f5a2b9462b57bf847e82a40f7a3
Grant Reighard <grant.reighard@gmail.com>
2024-01-17 18:08:52 -0500

    Adds book json and endpoint

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express from "express";
+import booksJson from "./data/books.json";
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT ?? 4000 // add PORT from process.env for use on Heroku for instance
+const port = process.env.PORT ?? 4000; // add PORT from process.env for use on Heroku for instance
+
+app.get("/books", (req, res) => {
+  res.status(200).send(booksJson);
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`)
-})
+  console.log(`Server listening on http://localhost:${port}`);
+});
