7fe60bad27e88cef9730eaaec282c2dd4cd824e4
Grant Reighard <grant.reighard@gmail.com>
2024-01-17 15:10:40 -0500

    Adds eslint to backend

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 4000; // add PORT from process.env for use on Heroku for instance
+const port = process.env.PORT ?? 4000 // add PORT from process.env for use on Heroku for instance
 
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
-});
+  console.log(`Server listening on http://localhost:${port}`)
+})
