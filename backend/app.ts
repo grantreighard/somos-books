68a9d43f28d96015564d369d59ce3768a678d755
Grant Reighard <grant.reighard@gmail.com>
2024-01-17 15:00:33 -0500

    Initial commit w/ CRA && node

---
+++ backend/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT || 4000; // add PORT from process.env for use on Heroku for instance
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
