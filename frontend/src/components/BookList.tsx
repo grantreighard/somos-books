5e614eaa49ed84cebed583d0abf280fd58980094
Grant Reighard <grant.reighard@gmail.com>
2024-01-18 12:56:25 -0500

    Fixed dimension divs; adds title

--- frontend/src/components/BookList.tsx
+++ frontend/src/components/BookList.tsx
@@ -6,22 +6,21 @@ const BookList = () => {
   const { books } = useContext(AppContext) as AppContextType;
   return (
     <div>
-      Book List
       <p>Search through our library of {books.length} books!</p>
       <div className="flex flex-wrap">
-        {books.map((book: IBook, index) => {
+        {books.map((book: IBook) => {
           return (
-            <div className="w-44 h-64 flex flex-col m-4 overflow-hidden">
+            <div className="w-[200px] h-[350px] flex flex-col m-4 overflow-hidden">
               <img
                 src={
                   book.thumbnailUrl ||
                   "https://placehold.co/450x570?text=No+book+cover"
                 }
-                width={150}
+                width={190}
                 alt={book.title}
               />
-              <p className="overflow-hidden truncate w-40">{book.title}</p>
-              <p>By {book.authors.join(", ") || "unknown author"}</p>
+              <p className="overflow-hidden truncate w-40" title={book.title}>{book.title}</p>
+              <p>By {book.authors.filter(a => !!a).slice(0, 3).join(", ") || "unknown author"}</p>
             </div>
           );
         })}
