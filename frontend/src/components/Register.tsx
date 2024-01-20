42df5d7797c21d4f22baae47132f8d7b362042c5
Grant Reighard <grant.reighard@gmail.com>
2024-01-19 19:16:12 -0500

    Fix warnings

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../contexts/appContext";
 import { AppContextType } from "../@types/context";
@@ -14,7 +14,8 @@ const Register = () => {
   const SPECIAL_REGEX = "^(?=.*[@$!%*?&#])";
   const LENGTH_REGEX = "^[A-Za-z0-9@$!%*?&#]{12,16}";
   const EMAIL_REGEX =
-    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    // eslint-disable-next-line
+    useMemo(() => /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/, []);
 
   const navigate = useNavigate();
   const { theme, setIsAuthenticated, setIsLoading, setEmail } = useContext(AppContext) as AppContextType;
@@ -54,7 +55,7 @@ const Register = () => {
     } else {
       setEmailFieldError("");
     }
-  }, [emailField]);
+  }, [emailField, EMAIL_REGEX]);
 
   const submitRegistration = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
